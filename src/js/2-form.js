2ebcbcb63656c8c1f16faa1738e8f529329c9427
Andrii Nefodov <gastronamixi@gmail.com>
2026-02-01 23:16:18 +0200

    add hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const formData = { 
+    email: "",
+    message: "",
+};
+
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", onFormInput);
+form.addEventListener("submit", handleSubmit)
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if(savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+function onFormInput(event) {
+const name = event.target.name;
+const value = event.target.value.trim();
+
+if(name === "email") {
+    formData.email = value;
+}else if(name === "message") {
+    formData.message = value;
+}
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+};
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if(formData.email === "" || formData.message === "") {
+        alert("Fill please all fields")
+        return
+    } 
+        localStorage.removeItem(STORAGE_KEY);
+        form.reset();
+        formData.email = "";
+        formData.message = "";
+    
+};
